c29d76183b0c0329d4070619b1bf6e881082476f
Azzy <AzzyBunn+github@gmail.com>
2024-11-29 19:49:06 +0100

    missing lang updated

--- src/settings.lua
+++ src/settings.lua
@@ -1,12 +1,5 @@
 data:extend({
 	{
-		type = "bool-setting",
-		name = "inserter-config-remove-long-inserters",
-		order = "a",
-		setting_type = "startup",
-		default_value = true
-	},
-	{
 		type = "string-setting",
 		name = "inserter-config-length-blacklist",
 		order = "b-a",
